b55215ef0cb9d044a2d6b6de024df5b7176ca813
EC2 Default User <ec2-user@ip-10-1-0-106.ap-northeast-1.compute.internal>
2024-09-03 16:18:06 +0000

    music-search

---
+++ app.rb
@@ -0,0 +1,20 @@
+require "bundler/setup"
+Bundler.require
+require "sinatra/reloader" if development?
+require "json"
+require "httparty"
+
+get '/' do
+  erb :index
+end
+
+get '/search' do
+    keyword = params[:keyword]
+    query = { term: keyword, country: "jp", media: "music", limit: 6 }
+    response = HTTParty.get("https://itunes.apple.com/search", query: query)
+    puts response.body
+    returned_json = JSON.parse(response.body)
+    @musics = returned_json['results']
+    erb :result
+end
+
